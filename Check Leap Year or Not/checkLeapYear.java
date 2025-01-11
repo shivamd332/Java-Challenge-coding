fee0bbe18dc2089566f687e89cdbe1a0552099e4
Shivam Dubey <134126380+shivamd332@users.noreply.github.com>
2025-01-11 13:53:02 +0530

    Add files via upload

---
+++ Check Leap Year or Not/checkLeapYear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class checkLeapYear {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a year for check leap year or not: ");
+    int year = sc.nextInt();
+
+    if(year % 400 == 0 || (year %4 ==0 && year % 100 != 0)){
+      System.out.println("it is leap year");
+    }
+    else {
+      System.out.println("not leap year");
+    }
+    sc.close();
+  }
+}
